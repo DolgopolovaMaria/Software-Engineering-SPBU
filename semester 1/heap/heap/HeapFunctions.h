0cc6e0f2d05b9cf52e89248463e4c0bdc7dad530
Dolgopolova Maria <dolgopolovamariia@gmail.com>
2018-02-19 22:15:11 +0500

    tasks

--- semester 1/heap/heap/HeapFunctions.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-int addValues(int heap[], int *heapSize, int arraySize, int value);
-
-int buildMaxHeap(int arrayForHeap[], int length);
-
-void heapSort(int arrayForSort[], int length);
